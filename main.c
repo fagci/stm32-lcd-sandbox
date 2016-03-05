287a69ab17aab11a924827a2ec0b1df5e0920d48
fagcinsk <fagci.nsk@gmail.com>
2016-03-05 15:33:00 +0600

    debug GRAM read

--- main.c
+++ main.c
@@ -5,16 +5,21 @@ int main(void) {
     LCD_init();
     usartInit();
 
+    u16 testColor = 0xACE0;
+
     usartPrintLn("BEGIN");
-    LCD_fillRect(0, 0, LCD_getWidth(), LCD_getHeight(), RED);
+    LCD_fillRect(0, 0, LCD_getWidth(), LCD_getHeight(), testColor);
 
     u16 c[1];
     usartPrintLn("start read");
+
     LCD_readPixels(0, 0, 0, 0, c);
+    if (c[0] == testColor) usartPrintLn("COLORS SAME!");
+    usartPrint("0x"), usartPrintNum(c[0], 16), usartPrintLn("");
+
     usartPrintLn("end read");
 
     LCD_fillRect(0, 0, LCD_getWidth(), LCD_getHeight(), GREEN);
-    LCD_fillRect(0, 0, LCD_getWidth(), LCD_getHeight(), BLUE);
     usartPrintLn("END");
     while (1);
 }
